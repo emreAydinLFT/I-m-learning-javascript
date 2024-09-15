4ed15bc96eaeec191073b6e73241180633604d29
emreAydinLFT <ea991461@gmail.com>
2024-09-15 14:04:16 +0300

    closure-repeat

---
+++ .history/JS EXERCISE/insta/script_20240912200921.js
@@ -0,0 +1,4 @@
+const sayilar = [1, 2, 3, 4, 5];
+const ikiKat = sayilar.map(sayi => sayi * 2);
+
+console.log(ikiKat); // [2, 4, 6, 8, 10]
---
+++ .history/JS EXERCISE/insta/script_20240912202604.js
@@ -0,0 +1,5 @@
+
+const sayilar = [1, 2, 3, 4, 5];
+const ikiKat = sayilar.map(sayi => sayi * 2);
+
+console.log(ikiKat); // [2, 4, 6, 8, 10]
